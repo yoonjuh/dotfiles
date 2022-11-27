6cad7093b4ca4cecde775442509c24f2707f6e99
Edwin Yoon <yoonjuh7@gmail.com>
2022-11-26 23:03:37 -0800

    Fix incomplete lsp setup by porting lsp installer setup to mason

--- nvim/lua/config/mason-nvim.lua
+++ nvim/lua/config/mason-nvim.lua
@@ -1,13 +1,12 @@
 -- https://github.com/williamboman/mason.nvim
 
-
 require("mason").setup({
   ui = {
-      icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-      }
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
   }
 })
 
@@ -22,7 +21,6 @@ require("mason-lspconfig").setup({
     "gopls",
     "golangci_lint_ls",
     "graphql",
-    "groovyls",
     "html",
     "jsonls",
     "tsserver",
@@ -37,25 +35,60 @@ require("mason-lspconfig").setup({
   automatic_installation = true,
 })
 
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>d', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 require("mason-lspconfig").setup_handlers {
-    -- The first entry (without a key) will be the default handler
+  -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
-  function (server_name) -- default handler (optional)
-      require("lspconfig")[server_name].setup { capabilities = capabilities }
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup { capabilities = capabilities, on_attach = on_attach }
   end,
   -- Next, you can provide a dedicated handler for specific servers.
   -- For example, a handler override for the `rust_analyzer`:
-  ["rust_analyzer"] = function ()
+  ["rust_analyzer"] = function()
     local rust_tools = require("rust-tools")
     rust_tools.setup({
       capabilities = capabilities,
+      on_attach = on_attach,
       server = {
         on_attach = function(_, bufnr)
-           -- Hover actions
+          -- Hover actions
           vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
           -- Code action groups
           vim.keymap.set("n", "<Leader>a", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
@@ -67,16 +100,17 @@ require("mason-lspconfig").setup_handlers {
   ["sumneko_lua"] = function(server_name)
     require("lspconfig")[server_name].setup({
       capabilities = capabilities,
-      default_opts = {
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { 'vim' }
-            }
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' }
           }
         }
       }
     })
   end
 }
+
+vim.lsp.set_log_level("debug")
 
