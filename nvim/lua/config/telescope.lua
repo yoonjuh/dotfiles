2da3e7ef0eae639e3d64ed3ff4a1a570aec1cc70
Edwin Yoon <yoonjuh7@gmail.com>
2023-02-20 15:51:18 -0800

    Update telescope find files command to display hidden files

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,7 +1,13 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
+local dotfiles_dir = "~/dotfiles"
 
 local telescope = require('telescope')
+local telescopeConfig = require("telescope.config")
+
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+table.insert(vimgrep_arguments, "--hidden")
+
 telescope.setup({
   extensions = {
     repo = {
@@ -11,27 +17,39 @@ telescope.setup({
       "~/dotfiles",
       "~/projects"
     }
+  },
+  pickers = {
+    find_files = {
+      find_command = {
+        -- This way, it will display the hiddne files as well
+        "rg",
+        "--files",
+        "--hidden",
+        "--glob",
+        "!**/.git/*"
+      },
+    },
   }
 })
 
+-- BUILTIN MAPPINGS
 local keymap = vim.api.nvim_set_keymap
-keymap('n', '<c-s>', ':w<CR>', {})
-
-
 local options = { noremap = true, silent = true }
 
-
--- BUILTIN MAPPINGS
 keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", options)
 keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", options)
 keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", options)
 keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", options)
-keymap('n', '<leader>fo', "<cmd>lua require('telescope.builtin').find_files({ cwd='/home/edwin/onelocal'})<cr>", options)
 keymap('n', '<leader>co', "<cmd>lua require('telescope.builtin').colorscheme()<cr>", options)
+
+-- This is where I usually put my dotfiles
+keymap('n', '<leader>fc', "<cmd>lua require('telescope.builtin').find_files({ cwd='" .. dotfiles_dir .. "'})<cr>", options)
+
+-- Save alias
+keymap('n', '<c-s>', ':w<CR>', {})
 
 
 -- PLUGIN MAPPINGS
-
 -- REPO
 keymap('n', '<leader>rl', "<cmd>lua require('telescope').extensions.repo.list{}<cr>", options)
 
