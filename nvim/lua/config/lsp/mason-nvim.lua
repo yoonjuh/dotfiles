8b49e4dd89b1bed4a16c290eb64125e17a58f3f1
Edwin Yoon <yoonjuh7@gmail.com>
2023-02-24 08:29:04 -0800

    Clean up lsp config and add null-ls setup

--- nvim/lua/config/lsp/mason-nvim.lua
+++ nvim/lua/config/lsp/mason-nvim.lua
@@ -5,9 +5,9 @@ require("mason").setup({
     icons = {
       package_installed = "✓",
       package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  }
+      package_uninstalled = "✗",
+    },
+  },
 })
 
 require("mason-lspconfig").setup({
@@ -27,37 +27,24 @@ require("mason-lspconfig").setup({
     "lua_ls",
     "taplo",
     "terraformls",
+    "tflint",
     "tsserver",
     "yamlls",
-    "zls"
+    "zls",
   },
   automatic_installation = true,
 })
 
 require("mason-tool-installer").setup({
   ensure_installed = {
-      -- LSP
-    "bash-language-server",
-    "dockerfile-language-server",
-    "json-lsp",
-    "marksman",
-    "typescript-language-server",
-    "texlab",
-    "lua-language-server",
-    "pyright",
-    "terraform-ls",
-    "yaml-language-server",
-    -- Formatter
+    -- Only install third party tools that mason-lspconfig does not supports
     "black",
     "prettier",
     "stylua",
-    -- Linter
     "eslint_d",
     "shellcheck",
-    "tflint",
     "vale",
     "yamllint",
-    -- DAP
     "debugpy",
   },
   -- if set to true this will check each tool for updates. If updates
@@ -65,13 +52,11 @@ require("mason-tool-installer").setup({
   -- affect :MasonToolsUpdate or :MasonToolsInstall.
   -- Default: false
   auto_update = false,
-
   -- automatically install / update on startup. If set to false nothing
   -- will happen on startup. You can use :MasonToolsInstall or
   -- :MasonToolsUpdate to install tools and check for updates.
   -- Default: true
   run_on_start = true,
-
   -- set a delay (in ms) before the installation starts. This is only
   -- effective if run_on_start is set to true.
   -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
@@ -82,44 +67,18 @@ require("mason-tool-installer").setup({
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>d', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
--- local on_attach = function(client, bufnr)
---   -- Enable completion triggered by <c-x><c-o>
---   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---
---   -- Mappings.
---   -- See `:help vim.lsp.*` for documentation on any of the below functions
---   local bufopts = { noremap = true, silent = true, buffer = bufnr }
---   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
---   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
---   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
---   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
---   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
---   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
---   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
---   vim.keymap.set(
---     'n',
---     '<space>wl',
---     function()
---       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---     end, bufopts)
---  nvim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
---   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
---   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
---   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
---   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
--- end
-
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>d", vim.diagnostic.setloclist, opts)
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
+  if client.name == "lua_ls" then
+    -- Formatting will be done through stylua but not lau-language-server
+    client.server_capabilities.document_formatting = false
+  end
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
   -- many times.
@@ -128,53 +87,52 @@ local on_attach = function(_, bufnr)
   -- for LSP related items. It sets the mode, buffer and description for us each time.
   local nmap = function(keys, func, desc)
     if desc then
-      desc = 'LSP: ' .. desc
+      desc = "LSP: " .. desc
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
   -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
+  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+  nmap("<leader>wl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+  end, "[W]orkspace [L]ist Folders")
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
     vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  end, { desc = "Format current buffer with LSP" })
 end
 
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-require("mason-lspconfig").setup_handlers {
+require("mason-lspconfig").setup_handlers({
   -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
   function(server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup { capabilities = capabilities, on_attach = on_attach }
+    require("lspconfig")[server_name].setup({ capabilities = capabilities, on_attach = on_attach })
   end,
   -- Next, you can provide a dedicated handler for specific servers.
-  -- For example, a handler override for the `rust_analyzer`:
+
   ["rust_analyzer"] = function()
     local rust_tools = require("rust-tools")
     rust_tools.setup({
@@ -186,11 +144,10 @@ require("mason-lspconfig").setup_handlers {
           vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
           -- Code action groups
           vim.keymap.set("n", "<Leader>a", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
-        end
-      }
+        end,
+      },
     })
   end,
-
   ["lua_ls"] = function(server_name)
     require("lspconfig")[server_name].setup({
       capabilities = capabilities,
@@ -198,10 +155,10 @@ require("mason-lspconfig").setup_handlers {
       settings = {
         Lua = {
           runtime = {
-            version = 'LuaJIT',
+            version = "LuaJIT",
           },
           diagnostics = {
-            globals = { 'vim' }
+            globals = { "vim" },
           },
           telemetry = {
             enable = false,
@@ -210,7 +167,6 @@ require("mason-lspconfig").setup_handlers {
       },
     })
   end,
-}
+})
 
 vim.lsp.set_log_level("debug")
-
