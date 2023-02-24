27a857176ab593643222c94f52fc4e9d90e0430c
Edwin Yoon <yoonjuh7@gmail.com>
2023-02-24 08:30:07 -0800

    Update lint setup for lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,7 @@
 -- Check if tree-sitter is installed before you use this config
 -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation
 
-require('keymaps')
-require('plugins')
-require('lsp')
-require('options')
-require('themes.dracula-nvim')
-
+require("keymaps")
+require("plugins")
+require("options")
+require("themes.dracula-nvim")
