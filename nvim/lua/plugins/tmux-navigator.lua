6872c0128506220c5f20347375ab60d8cbb24841
yoonjuh <yoonjuh7@gmail.com>
2025-01-28 19:55:41 -0800

    Fix navigation issue from vim to tmux

--- nvim/lua/plugins/tmux-navigator.lua
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -2,18 +2,17 @@ return {
   {
     "alexghergh/nvim-tmux-navigation",
     config = function()
-      local nvim_tmux_nav = require("nvim-tmux-navigation")
-
-      nvim_tmux_nav.setup({
+      require("nvim-tmux-navigation").setup({
         disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
       })
-
-      vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-      vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-      vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-      vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-      vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-      vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
     end,
   },
 }
